51005045d3a6b0ea355c8794beef5145d7745c5a
Orekarako <orekarako@gmail.com>
2023-05-08 13:02:43 +0300

    Update login.ts

--- public/src/login.ts
+++ public/src/login.ts
@@ -1,10 +1,10 @@
 // Set Background Music 
-const  backgroundSound =  new Audio()
+const backgroundSound = new Audio()
 backgroundSound.src = '../audio/backgroundMusicRegisterLogin.mp3'
 
 
 
-function onLoad(){
+function onLoad() {
     try {
     } catch (error) {
         console.error(error)
@@ -13,32 +13,32 @@ function onLoad(){
 
 // handle functions
 
-function handleClickMusicBtn(){
+function handleClickMusicBtn() {
     try {
-       
-        const btnPlay:HTMLElement | null = document.querySelector('#btn-play-pause img');
-        if(!btnPlay) throw new Error("Button Play element no exists ");
-       btnPlay.classList.toggle("active")   
-        if(btnPlay.classList.contains('active'))
-        backgroundSound.pause()
+
+        const btnPlay: HTMLElement | null = document.querySelector('#btn-play-pause img');
+        if (!btnPlay) throw new Error("Button Play element no exists ");
+        btnPlay.classList.toggle("active")
+        if (btnPlay.classList.contains('active'))
+            backgroundSound.pause()
         else
- backgroundSound.play()
+            backgroundSound.play()
 
     } catch (error) {
         console.error(error)
     }
 }
 
-function handleSubmitLoginForm(ev:any) {
+function handleSubmitLoginForm(ev: any) {
     try {
         ev.preventDefault()
-        const {userName, password } = ev.target.elements
-        
+        const { userName, password } = ev.target.elements
+
         console.log(
             userName.value,
             password.value,
 
-            );
+        );
 
 
     } catch (error) {
