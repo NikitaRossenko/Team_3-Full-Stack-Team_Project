a5449f0679daf05369f68644937c468fa68c3fcd
Nikita Rossenko <darkcaty1999@live.ru>
2023-05-11 19:27:22 +0300

    1

--- public/src/game/dist/game.js
+++ public/src/game/dist/game.js
@@ -1,11 +1,12 @@
 function game() {
     try {
         var mapSize_1 = 1.5;
+        var tileSize_1 = 12;
         var monsterSpeed = 200;
         var enemies_1 = [];
         var placementTowersArray_1 = [];
         var canvas_1 = document.querySelector("canvas");
-        var ctx_1 = canvas_1 === null || canvas_1 === void 0 ? void 0 : canvas_1.getContext('2d');
+        var ctx_1 = canvas_1 === null || canvas_1 === void 0 ? void 0 : canvas_1.getContext("2d");
         var placementTowers2d = [];
         if (!canvas_1)
             throw new Error("[Canvas] Game Error");
@@ -20,7 +21,8 @@ function game() {
         ctx_1.fillRect(0, 0, canvas_1.width, canvas_1.height);
         // Need to declare a new image (which will create an img element) - canvas need to receive a img element
         var mapImage_1 = new Image();
-        mapImage_1.src = "../../images/maps/Road-Of-Glory-peaceful-Map_1260x720.png";
+        mapImage_1.src =
+            "../../images/maps/Road-Of-Glory-peaceful-Map_1260x720.png";
         // Convert Towers coordinats to 2d
         for (var i = 0; i < placementTowers.length; i += 70) {
             placementTowers2d.push(placementTowers.slice(i, i + 70));
@@ -29,7 +31,7 @@ function game() {
             function PlacementTower(_a) {
                 var _b = _a.x, x = _b === void 0 ? 0 : _b, _c = _a.y, y = _c === void 0 ? 0 : _c;
                 this.position = { x: x, y: y };
-                this.size = 24;
+                this.size = 18;
                 this.color = "rgba(128,0,128,0.2)";
                 console.log(this.position);
             }
@@ -41,7 +43,10 @@ function game() {
             };
             PlacementTower.prototype.update = function (mousePos) {
                 this.draw();
-                if (mousePos.x > this.position.x && mousePos.x < this.position.x + this.size && mousePos.y > this.position.y && mousePos.y < this.position.y + this.size) {
+                if (mousePos.x > this.position.x &&
+                    mousePos.x < this.position.x + this.size &&
+                    mousePos.y > this.position.y &&
+                    mousePos.y < this.position.y + this.size) {
                     this.color = "rgba(128,0,128,1)";
                 }
                 else {
@@ -58,7 +63,10 @@ function game() {
                 this.height = 24;
                 this.waypointIndex = 0;
                 this.zoom = mapSize_1;
-                this.center = { x: this.position.x + this.width / 2, y: this.position.y + this.height / 2 };
+                this.center = {
+                    x: this.position.x + this.width / 2,
+                    y: this.position.y + this.height / 2
+                };
             }
             Enemey.prototype.draw = function () {
                 if (!ctx_1)
@@ -74,8 +82,13 @@ function game() {
                 var angle = Math.atan2(yWaypoint, xWaypoint);
                 this.position.x += Math.cos(angle);
                 this.position.y += Math.sin(angle);
-                this.center = { x: this.position.x + this.width / 2, y: this.position.y + this.height / 2 };
-                if (Math.round(this.center.x) === Math.round(waypoint.x) && Math.round(this.center.y) === Math.round(waypoint.y) && this.waypointIndex < path.length - 1) {
+                this.center = {
+                    x: this.position.x + this.width / 2,
+                    y: this.position.y + this.height / 2
+                };
+                if (Math.round(this.center.x) === Math.round(waypoint.x) &&
+                    Math.round(this.center.y) === Math.round(waypoint.y) &&
+                    this.waypointIndex < path.length - 1) {
                     this.waypointIndex++;
                 }
             };
@@ -84,7 +97,10 @@ function game() {
         placementTowers2d.forEach(function (row, y) {
             row.forEach(function (symbol, x) {
                 if (symbol === 1211) {
-                    placementTowersArray_1.push(new PlacementTower_1({ x: x * 24, y: y * 24 }));
+                    placementTowersArray_1.push(new PlacementTower_1({
+                        x: x * tileSize_1 * mapSize_1,
+                        y: y * tileSize_1 * mapSize_1
+                    }));
                 }
             });
         });
@@ -93,6 +109,13 @@ function game() {
             var xOffset = i * (Math.random() * (600 - 100 + 1) + 100);
             enemies_1.push(new Enemey({ x: path[i].x - xOffset, y: path[i].y }));
         }
+        // Monitor mouse event "move" to catch the coordinats and use it to find elements inside the canvas
+        var mousePos_1 = { x: undefined, y: undefined };
+        window.addEventListener("mousemove", function (event) {
+            mousePos_1.x = event.clientX - canvas_1.offsetLeft;
+            mousePos_1.y = event.clientY - canvas_1.offsetTop;
+            console.log(mousePos_1.x, mousePos_1.y);
+        });
         // Animation function (Recursion)
         function animate() {
             requestAnimationFrame(animate);
@@ -105,14 +128,9 @@ function game() {
             placementTowersArray_1.forEach(function (tower) {
                 tower.update(mousePos_1);
             });
+            console.log("mousePos.x", mousePos_1.x + canvas_1.offsetLeft, "mousePos.y", mousePos_1.y + canvas_1.offsetTop, "this.position.x", placementTowersArray_1[0].position.x, "this.position.y", placementTowersArray_1[0].position.y);
         }
-        // Monitor mouse event "move" to catch the coordinats and use it to find elements inside the canvas
-        var mousePos_1 = { x: undefined, y: undefined };
-        window.addEventListener('mousemove', function (event) {
-            mousePos_1.x = event.clientX - canvas_1.offsetLeft;
-            mousePos_1.y = event.clientY - canvas_1.offsetTop;
-            console.log(mousePos_1.x, mousePos_1.y);
-        });
+        console.log(placementTowersArray_1[0].position.x, placementTowersArray_1[0].position.y);
         animate();
     }
     catch (error) {
--- public/src/game/game.ts
+++ public/src/game/game.ts
@@ -1,156 +1,187 @@
-
 function game() {
     try {
-        const mapSize = 1.5
-        let monsterSpeed = 200
-        
-        const enemies:any = []
-        const placementTowersArray:any = []
+        const mapSize = 1.5;
+        const tileSize = 12
+        let monsterSpeed = 200;
 
-        const canvas = document.querySelector("canvas")
-        const ctx = canvas?.getContext('2d');
-        const placementTowers2d:any = []
+        const enemies: any = [];
+        const placementTowersArray: any = [];
 
-        if (!canvas) throw new Error("[Canvas] Game Error")
-        if (!ctx) throw new Error("[Canvas-ctx] Game Error")
+        const canvas = document.querySelector("canvas");
+        const ctx = canvas?.getContext("2d");
+        const placementTowers2d: any = [];
+
+        if (!canvas) throw new Error("[Canvas] Game Error");
+        if (!ctx) throw new Error("[Canvas-ctx] Game Error");
 
         // Set the canvas Width and Height
-        canvas.width = 1260
-        canvas.height = 720
+        canvas.width = 1260;
+        canvas.height = 720;
 
         // Canvas fill is optional if using a background image
-        ctx.fillStyle = "white"
+        ctx.fillStyle = "white";
 
         // If using a background image this fill is optional
-        ctx.fillRect(0,0,canvas.width,canvas.height)
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
 
         // Need to declare a new image (which will create an img element) - canvas need to receive a img element
-        const mapImage = new Image()
-        mapImage.src = "../../images/maps/Road-Of-Glory-peaceful-Map_1260x720.png"
+        const mapImage = new Image();
+        mapImage.src =
+            "../../images/maps/Road-Of-Glory-peaceful-Map_1260x720.png";
 
         // Convert Towers coordinats to 2d
-        for (let i = 0 ; i < placementTowers.length ; i+=70){
-            placementTowers2d.push(placementTowers.slice(i,i+70))
+        for (let i = 0; i < placementTowers.length; i += 70) {
+            placementTowers2d.push(placementTowers.slice(i, i + 70));
         }
 
         class PlacementTower {
-            position: { x: number; y: number; };
+            position: { x: number; y: number };
             size: number;
             color: string;
-            constructor({x=0, y=0}){
-                this.position = {x:x, y:y}
-                this.size = 24
-                this.color = "rgba(128,0,128,0.2)"
-                console.log(this.position)
+            constructor({ x = 0, y = 0 }) {
+                this.position = { x: x, y: y };
+                this.size = 18;
+                this.color = "rgba(128,0,128,0.2)";
+                console.log(this.position);
             }
-            
 
             draw() {
-                if (!ctx) throw new Error("[Canvas-ctx] Game Error")
+                if (!ctx) throw new Error("[Canvas-ctx] Game Error");
 
-                ctx.fillStyle = this.color
-                ctx.fillRect(this.position.x, this.position.y, this.size, this.size)
-
+                ctx.fillStyle = this.color;
+                ctx.fillRect(
+                    this.position.x,
+                    this.position.y,
+                    this.size,
+                    this.size
+                );
             }
 
-            update(mousePos){
-                this.draw()
-                if (mousePos.x > this.position.x && mousePos.x < this.position.x + this.size  && mousePos.y > this.position.y && mousePos.y < this.position.y + this.size){
-                    this.color = "rgba(128,0,128,1)"
+            update(mousePos) {
+                this.draw();
+                if (
+                    mousePos.x > this.position.x &&
+                    mousePos.x < this.position.x + this.size &&
+                    mousePos.y > this.position.y &&
+                    mousePos.y < this.position.y + this.size
+                ) {
+                    this.color = "rgba(128,0,128,1)";
                 } else {
-                    this.color = "rgba(128,0,128,0.2)"
+                    this.color = "rgba(128,0,128,0.2)";
                 }
             }
         }
 
         class Enemey {
-            position:{x:number; y:number;}
+            position: { x: number; y: number };
             width: number;
             height: number;
             waypointIndex: number;
             zoom: number;
-            center: { x: number; y: number; };
+            center: { x: number; y: number };
 
-            constructor({x=0,y=0}){
-                this.position = {x:x, y:y}
-                this.width = 24
-                this.height = 24
-                this.waypointIndex = 0
-                this.zoom = mapSize
-                this.center = {x:this.position.x + this.width/2, y:this.position.y + this.height/2}
+            constructor({ x = 0, y = 0 }) {
+                this.position = { x: x, y: y };
+                this.width = 24;
+                this.height = 24;
+                this.waypointIndex = 0;
+                this.zoom = mapSize;
+                this.center = {
+                    x: this.position.x + this.width / 2,
+                    y: this.position.y + this.height / 2,
+                };
             }
 
-            draw(){
-                if (!ctx) throw new Error("[Canvas-ctx] Game Error")
-                ctx.fillStyle = "purple"
-                ctx.fillRect(this.position.x*this.zoom, this.position.y*this.zoom, this.width, this.height)
+            draw() {
+                if (!ctx) throw new Error("[Canvas-ctx] Game Error");
+                ctx.fillStyle = "purple";
+                ctx.fillRect(
+                    this.position.x * this.zoom,
+                    this.position.y * this.zoom,
+                    this.width,
+                    this.height
+                );
             }
 
             update() {
-                this.draw()
-                const waypoint = path[this.waypointIndex]
-                
-                const yWaypoint = waypoint.y - this.center.y
-                const xWaypoint = waypoint.x - this.center.x
-                const angle = Math.atan2(yWaypoint, xWaypoint)
-                this.position.x += Math.cos(angle)
-                this.position.y += Math.sin(angle)
-                this.center = {x:this.position.x + this.width/2, y:this.position.y + this.height/2}
-                
+                this.draw();
+                const waypoint = path[this.waypointIndex];
 
-                if (Math.round(this.center.x) === Math.round(waypoint.x) && Math.round(this.center.y) === Math.round(waypoint.y) && this.waypointIndex < path.length-1){
-                    this.waypointIndex++
+                const yWaypoint = waypoint.y - this.center.y;
+                const xWaypoint = waypoint.x - this.center.x;
+                const angle = Math.atan2(yWaypoint, xWaypoint);
+                this.position.x += Math.cos(angle);
+                this.position.y += Math.sin(angle);
+                this.center = {
+                    x: this.position.x + this.width / 2,
+                    y: this.position.y + this.height / 2,
+                };
+
+                if (
+                    Math.round(this.center.x) === Math.round(waypoint.x) &&
+                    Math.round(this.center.y) === Math.round(waypoint.y) &&
+                    this.waypointIndex < path.length - 1
+                ) {
+                    this.waypointIndex++;
                 }
             }
         }
 
         placementTowers2d.forEach((row, y) => {
             row.forEach((symbol, x) => {
-                if (symbol === 1211){
-                    placementTowersArray.push(new PlacementTower({x:x*24, y:y*24}))
+                if (symbol === 1211) {
+                    placementTowersArray.push(
+                        new PlacementTower({
+                            x: x * tileSize * mapSize,
+                            y: y * tileSize * mapSize,
+                        })
+                    
+                    );
                 }
-            })
-        })
+            });
+        });
 
         // Create enemies with X coordinats offset
-        for (let i = 1; i < 10 ; i++){
-            const xOffset = i * (Math.random()*(600 - 100 +1) + 100)
-            enemies.push(new Enemey({x:path[i].x - xOffset, y:path[i].y}))
-        }
-
-        // Animation function (Recursion)
-        function animate(){
-            requestAnimationFrame(animate)
-            if (!ctx) throw new Error("[Canvas-ctx] Game Error")
-
-            ctx.drawImage(mapImage,0,0)
-            
-            enemies.forEach( enemy => {
-                enemy.update()
-            })
-
-            placementTowersArray.forEach(tower => {
-                tower.update(mousePos)
-            })
-
-            
+        for (let i = 1; i < 10; i++) {
+            const xOffset = i * (Math.random() * (600 - 100 + 1) + 100);
+            enemies.push(new Enemey({ x: path[i].x - xOffset, y: path[i].y }));
         }
 
         // Monitor mouse event "move" to catch the coordinats and use it to find elements inside the canvas
-        const mousePos:any = {x:undefined, y:undefined}
-        window.addEventListener('mousemove', (event) => {
-            mousePos.x = event.clientX - canvas.offsetLeft
-            mousePos.y = event.clientY - canvas.offsetTop
-            console.log(mousePos.x,mousePos.y)
+        const mousePos: any = { x: undefined, y: undefined };
+        window.addEventListener("mousemove", (event) => {
+            mousePos.x = event.clientX - canvas.offsetLeft;
+            mousePos.y = event.clientY - canvas.offsetTop;
+            console.log(mousePos.x, mousePos.y);
+        });
 
-        })
+        // Animation function (Recursion)
+        function animate() {
+            requestAnimationFrame(animate);
+            if (!ctx) throw new Error("[Canvas-ctx] Game Error");
 
-        animate()
+            ctx.drawImage(mapImage, 0, 0);
+
+            enemies.forEach((enemy) => {
+                enemy.update();
+            });
+
+            placementTowersArray.forEach((tower) => {
+                tower.update(mousePos);
+            });
+            console.log("mousePos.x",mousePos.x + canvas.offsetLeft,"mousePos.y",mousePos.y + canvas.offsetTop, "this.position.x",placementTowersArray[0].position.x, "this.position.y",placementTowersArray[0].position.y)
+        }
+
+        console.log(
+            placementTowersArray[0].position.x,
+            placementTowersArray[0].position.y
+        );
+        animate();
 
 
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
 }
 
-game()
+game();
