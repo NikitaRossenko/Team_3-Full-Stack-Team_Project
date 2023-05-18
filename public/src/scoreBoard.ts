ac4a514fa35dbf8d6f2c4c0219b7831eacd32097
itayz1e <itayz1e@gmail.com>
2023-05-18 22:57:26 +0300

    fix render scoreBoard

--- public/src/scoreBoard.ts
+++ public/src/scoreBoard.ts
@@ -17,7 +17,7 @@ async function renderScoreUser() {
       const html = `
       <li class="person">
         <p class="icon"><img class="playericons" src=${user.src}></p>
-        <p class="nickname">#2 - ${user.userName}</p>
+        <p class="nickname">${user.userName}</p>
         <p class="score">${user.highScore?user.highScore:0}</p>
         <ul class="point-btns"></ul>
       </li>
